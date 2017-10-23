17cb2f35439205ccf384ec58c255660f1045710d
Michal Jakubow <210326@student.pwr.edu.pl>
2017-10-23 18:47:44 +0200

    Moje wspaniałe rozwiązanie

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :posts
+  resources :topics
+  resources :courses
+  resources :students
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root to: 'static#index'
 end
